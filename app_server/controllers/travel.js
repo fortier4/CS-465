e9d89d9babd27e51027f561a6fa4b29374dac418
fortier4 <connor.fortier@snhu.edu>
2022-05-19 19:13:12 -0400

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,14 @@
 //Connor Fortier
 
-/* get travel view */
+//use built in node js file system component with fs.readFileSync() method
+//to retrieve data just created
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+//get travel view
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });  //pass trip data to hbs view
 };
 
 module.exports = {
